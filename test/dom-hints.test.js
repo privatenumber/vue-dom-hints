f9961f144f3a4d086d0e6f0a188a9f3b195c952b
Hiroki Osame <hiroki.osame@gmail.com>
2020-08-04 17:33:00 -0400

    tests: more tests

--- test/dom-hints.test.js
+++ test/dom-hints.test.js
@@ -5,30 +5,55 @@ let Foo;
 
 beforeEach(() => {
 	Foo = {
+		__file: 'Foo.vue',
+		name: 'Foo',
 		render(h) {
 			return h('div', ['foo']);
 		},
 	};
 });
 
-test('hint attribute annotation', () => {
-	const localVue = createLocalVue();
-	localVue.use(DomHints);
+describe('hint attribute annotation', () => {
 
-	Foo.name = `component${Math.random()}`;
+	test('use __file', () => {
+		const localVue = createLocalVue();
+		localVue.use(DomHints);
 
-	const wrapper = mount(Foo, {localVue});
-	expect(wrapper.vm.$el.getAttribute('__vue__')).toBe(JSON.stringify([Foo.name, 'App root']));
-});
+		const wrapper = mount(Foo, {localVue});
+		expect(wrapper.vm.$el.getAttribute('__vue__')).toBe(JSON.stringify([Foo.__file, 'App root']));
+	});
 
-test('custom annotation attribute', () => {
-	const localVue = createLocalVue();
-	localVue.use(DomHints, {attributeName: 'random-attribute'});
+	test('fallback to name if no __file', () => {
+		const localVue = createLocalVue();
+		localVue.use(DomHints);
 
-	Foo.name = `component${Math.random()}`;
+		delete Foo.__file;
 
-	const wrapper = mount(Foo, {localVue});
-	expect(wrapper.vm.$el.getAttribute('random-attribute')).toBe(JSON.stringify([Foo.name, 'App root']));
+		const wrapper = mount(Foo, {localVue});
+		expect(wrapper.vm.$el.getAttribute('__vue__')).toBe(JSON.stringify([Foo.name, 'App root']));
+	});
+
+	test('custom annotation attribute', () => {
+		const localVue = createLocalVue();
+		localVue.use(DomHints, {attributeName: 'random-attribute'});
+
+		const wrapper = mount(Foo, {localVue});
+		expect(wrapper.vm.$el.getAttribute('random-attribute')).toBe(JSON.stringify([Foo.__file, 'App root']));
+	});
+
+	test('extended component', () => {
+		const localVue = createLocalVue();
+		localVue.use(DomHints, {attributeName: 'random-attribute'});
+
+		const Bar = {
+			// Seems like with extends, the `__file` property needs to be overwritten explicitly
+			__file: 'Bar.vue',
+			extends: Foo,
+		};
+
+		const wrapper = mount(Bar, {localVue});
+		expect(wrapper.vm.$el.getAttribute('random-attribute')).toBe(JSON.stringify(['Bar.vue', 'App root']));
+	});
 });
 
 describe('error cases', () => {
@@ -37,6 +62,6 @@ describe('error cases', () => {
 		localVue.use(DomHints);
 
 		const wrapper = mount(Foo, {localVue});
-		expect(wrapper.vm.$el.getAttribute('__vue__')).toBe(JSON.stringify(['App root']));
+		expect(wrapper.vm.$el.getAttribute('__vue__')).toBe(JSON.stringify(['Foo.vue', 'App root']));
 	});
 });
